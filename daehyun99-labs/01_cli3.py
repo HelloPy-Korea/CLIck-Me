4cbe806222b80eeb62f9a75d3168a31d0ba2f903
daehyun99-labs <2018036042@chungbuk.ac.kr>
2025-08-05 18:42:40 +0900

    Feat: CLI 관련 라이브러리 실습 코드

---
+++ daehyun99-labs/01_cli3.py
@@ -0,0 +1,14 @@
+### 실습3 ###
+# 실행 명령어1: python [본인_이름]/01_cli3.py --name CLIck-Me
+# 실행 명령어2: python [본인_이름]/01_cli3.py --mbti ENFJ
+
+# ==========|코드 실습|========= #
+import click
+
+@click.command()
+
+def main():
+    ...
+    
+if __name__ == "__main__":
+    main()
---
+++ daehyun99-labs/01_cli7.py
@@ -0,0 +1,12 @@
+### 실습7 ###
+# 실행 명령어: python [본인_이름]/01_cli7.py
+
+# ==========|코드 실습|========= #
+import click
+
+@click.command()
+def main():
+    ...
+
+if __name__ == "__main__":
+    main()
